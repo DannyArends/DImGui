506a3eb07a19e4c84cae80559968bb6cf1b0e4f7
Danny Arends <Danny.Arends@gmail.com>
2025-03-31 18:24:53 +0100

    Output to ./bin/

--- compile.bat
+++ compile.bat
@@ -1,9 +1,11 @@
+echo off
 set vulkandir=%1
+set X=./deps/cimgui
+set Y=%X%/imgui
 call "C:/Program Files/Microsoft Visual Studio/2019/BuildTools/VC/Auxiliary/Build/vcvars64.bat"
-cl.exe /LD /DIMGUI_DISABLE_OBSOLETE_FUNCTIONS=1 /DIMGUI_IMPL_API="extern \"C\" __declspec(dllexport)" /DCIMGUI_USE_SDL2 /DCIMGUI_USE_VULKAN ^
-/I ./deps/cimgui/imgui /I ./deps/cimgui/imgui/backends/ /I %vulkandir%/Include /I %vulkandir%/Include/SDL2 ^
-./deps/cimgui/cimgui.cpp ./deps/cimgui/cimgui_impl.cpp ^
-./deps/cimgui/imgui/imgui.cpp ./deps/cimgui/imgui/imgui_draw.cpp ./deps/cimgui/imgui/imgui_demo.cpp ./deps/cimgui/imgui/imgui_tables.cpp ^
-./deps/cimgui/imgui/imgui_widgets.cpp ./deps/cimgui/imgui/backends/imgui_impl_vulkan.cpp ./deps/cimgui/imgui/backends/imgui_impl_sdl2.cpp ^
+cl.exe /LD /Fe:./bin/ /Fo:./bin/ /DIMGUI_DISABLE_OBSOLETE_FUNCTIONS=1 /DIMGUI_IMPL_API="extern \"C\" __declspec(dllexport)" /DCIMGUI_USE_SDL2 /DCIMGUI_USE_VULKAN ^
+/I %Y% /I %Y%/backends/ /I %vulkandir%/Include /I %vulkandir%/Include/SDL2 ^
+%X%/cimgui.cpp %X%/cimgui_impl.cpp %Y%/imgui.cpp %Y%/imgui_draw.cpp %Y%/imgui_demo.cpp %Y%/imgui_tables.cpp ^
+%Y%/imgui_widgets.cpp %Y%/backends/imgui_impl_vulkan.cpp %Y%/backends/imgui_impl_sdl2.cpp ^
 SDL2main.lib SDL2.lib vulkan-1.lib /link /LIBPATH:%vulkandir%/Lib
-rm *.obj
+rm ./bin/*.obj
