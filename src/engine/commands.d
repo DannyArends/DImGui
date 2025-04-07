c3d5d4dcc59facce3951b77c137401bb71b22394
DannyArends <Danny.Arends@gmail.com>
2025-04-07 10:33:55 +0100

    Use functions to create the commandPool and commandBuffer

--- src/engine/commands.d
+++ src/engine/commands.d
@@ -1,24 +1,39 @@
 import engine;
 
+VkCommandPool createCommandPool(VkDevice device, uint queueFamilyIndex) {
+  VkCommandPool commandPool;
+
+  VkCommandPoolCreateInfo poolInfo = {
+    sType: VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+    queueFamilyIndex: queueFamilyIndex,
+    flags: VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
+  };
+  enforceVK(vkCreateCommandPool(device, &poolInfo, null, &commandPool));
+  SDL_Log("Commandpool %p at queue %d created", commandPool, poolInfo.queueFamilyIndex);
+  return(commandPool);
+}
+
+VkCommandBuffer createCommandBuffer(VkDevice device, VkCommandPool commandPool, uint nBuffers = 1) {
+  VkCommandBuffer commandBuffer;
+
+  VkCommandBufferAllocateInfo allocInfo = {
+    sType: VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+    commandPool: commandPool,
+    level: VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+    commandBufferCount: nBuffers
+  };
+  enforceVK(vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer));
+  SDL_Log("%d CommandBuffer created for pool %p", allocInfo.commandBufferCount, commandPool);
+  return(commandBuffer);
+}
+
 void createCommandBuffers(ref App app){
   app.commandPool.length = app.imageCount;
   app.commandBuffers.length = app.imageCount;
 
-  VkCommandPoolCreateInfo poolInfo = {
-    sType : VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
-    flags : VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
-    queueFamilyIndex : app.queueFamily
-  };
   for (uint i = 0; i < app.imageCount; i++) {
-    enforceVK(vkCreateCommandPool(app.device, &poolInfo, null, &app.commandPool[i]));
-    SDL_Log("Command pool %p created", app.commandPool[i]);
-    VkCommandBufferAllocateInfo allocInfo = {
-      sType: VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
-      commandPool: app.commandPool[i],
-      level: VK_COMMAND_BUFFER_LEVEL_PRIMARY,
-      commandBufferCount: cast(uint) 1
-    };
-    enforceVK(vkAllocateCommandBuffers(app.device, &allocInfo, &app.commandBuffers[i]));
-    SDL_Log("Command buffer with %d buffers created", allocInfo.commandBufferCount);
+    app.commandPool[i] = app.device.createCommandPool(app.queueFamily);
+    app.commandBuffers[i] = app.device.createCommandBuffer(app.commandPool[i]);
   }
 }
+
