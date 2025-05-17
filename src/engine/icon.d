2877a9e142ff2b7dd4b6b33f24009eda070ace75
Danny Arends <Danny.Arends@gmail.com>
2025-05-17 09:16:45 +0100

    Setting a window icon

---
+++ src/engine/icon.d
@@ -0,0 +1,15 @@
+/** 
+ * Authors: Danny Arends
+ * License: GPL-v3 (See accompanying file LICENSE.txt or copy at https://www.gnu.org/licenses/gpl-3.0.en.html)
+ */
+import includes;
+
+import io : isfile;
+
+void setIcon(SDL_Window *window, const(char)* path = "assets/icons/icon.png") {
+  if (path.isfile()) {
+    SDL_Surface* surface = IMG_Load(path);
+    SDL_SetWindowIcon(window, surface);
+    SDL_FreeSurface(surface);
+  }
+}
