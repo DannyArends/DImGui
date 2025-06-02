709f9a4ac66bf6a2cd561eebfc68e7e66332ae92
DannyArends <Danny.Arends@gmail.com>
2025-06-02 09:47:25 +0100

    GDC doesn't like named arguments to a struct

--- src/engine/lights.d
+++ src/engine/lights.d
@@ -3,7 +3,7 @@
  * License: GPL-v3 (See accompanying file LICENSE.txt or copy at https://www.gnu.org/licenses/gpl-3.0.en.html)
  */
 
-public import includes;
+import includes;
 
 struct Light {
   float[4] position   = [0.0f, 0.0f, 0.0f, 0.0f];    /// Light Position
