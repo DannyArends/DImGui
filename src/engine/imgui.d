90a76aa4a1f716f6af2c52503aefbc8ce87ca21a
DannyArends <Danny.Arends@gmail.com>
2025-07-16 16:52:29 +0100

    MainMenu changes

--- src/engine/imgui.d
+++ src/engine/imgui.d
@@ -541,15 +541,22 @@ void showMenu(ref App app, uint font = 0) {
   igPushFont(app.gui.fonts[font]);
   if(igBeginMainMenuBar()) {
     if(igBeginMenu("File".toStringz, true)) {
+      if(igMenuItem_Bool("Open".toStringz,null, false, true)) {  app.gui.showDirectory = !app.gui.showDirectory; }
       if(igMenuItem_Bool("FPS".toStringz,null, false, true)) {  app.gui.showFPS = !app.gui.showFPS; }
       if(igMenuItem_Bool("Demo".toStringz,null, false, true)) {  app.gui.showDemo = !app.gui.showDemo; }
+
+      igEndMenu();
+    }
+    if(igBeginMenu("Edit".toStringz, true)) {
+      if(igMenuItem_Bool("Settings".toStringz,null, false, true)) { app.gui.showSettings = !app.gui.showSettings; }
       if(igMenuItem_Bool("Objects".toStringz,null, false, true)) { app.gui.showObjects = !app.gui.showObjects; }
       if(igMenuItem_Bool("Sounds".toStringz,null, false, true)) { app.gui.showSFX = !app.gui.showSFX; }
-      if(igMenuItem_Bool("Shaders".toStringz,null, false, true)) { app.gui.showShaders = !app.gui.showShaders; }
-      if(igMenuItem_Bool("Settings".toStringz,null, false, true)) { app.gui.showSettings = !app.gui.showSettings; }
       if(igMenuItem_Bool("Lights".toStringz,null, false, true)) { app.gui.showLights = !app.gui.showLights; }
+      igEndMenu();
+    }
+    if(igBeginMenu("View".toStringz, true)) {
+      if(igMenuItem_Bool("Shaders".toStringz,null, false, true)) { app.gui.showShaders = !app.gui.showShaders; }
       if(igMenuItem_Bool("Textures".toStringz,null, false, true)) {  app.gui.showTexture = !app.gui.showTexture; }
-      if(igMenuItem_Bool("Directory".toStringz,null, false, true)) {  app.gui.showDirectory = !app.gui.showDirectory; }
       igEndMenu();
     }
     igEndMainMenuBar();
