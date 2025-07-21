c09ee109c2267aed98ec83487b2f49416c867daa
Danny Arends <Danny.Arends@gmail.com>
2025-07-21 12:41:53 +0100

    No MSAA on Android

--- src/engine/devices.d
+++ src/engine/devices.d
@@ -23,7 +23,7 @@ void pickPhysicalDevice(ref App app, uint device = 0){
 
 VkSampleCountFlagBits getMSAASamples(const App app) {
   version (Android) {
-    return VK_SAMPLE_COUNT_4_BIT;
+    return VK_SAMPLE_COUNT_1_BIT;
   }
   VkSampleCountFlags counts = app.properties.limits.framebufferColorSampleCounts & app.properties.limits.framebufferDepthSampleCounts;
   if (counts & VK_SAMPLE_COUNT_64_BIT) { return VK_SAMPLE_COUNT_64_BIT; }
