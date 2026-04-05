3639ba77d7b5040486323b3011cdd92d09c5a5a9
DannyArends <Danny.Arends@gmail.com>
2026-04-05 17:36:05 +0100

    100 is enough

--- src/objects/camera.d
+++ src/objects/camera.d
@@ -16,7 +16,7 @@ struct Camera {
   alias capabilities this;
 
   float[3]        lookat      = [0.0f, 0.0f, 0.0f];     /// Position in the middle of the screen
-  float[2]        nearfar     = [0.1f, 1000.0f];         /// View distances, near [0], far [1]
+  float[2]        nearfar     = [0.1f, 100.0f];         /// View distances, near [0], far [1]
   float[3]        up          = [0.0f, 1.0f, 0.0f];     /// Defined up vector
   float           fov         = 45.0f;                  /// Field of view
   float           speed       =  0.1f;                  /// Movement speed
