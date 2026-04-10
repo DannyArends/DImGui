6e696a2c01deb29bf589d76a74923c323ee23b34
DannyArends <Danny.Arends@gmail.com>
2026-04-10 14:13:30 +0100

    Evict blocks (as well as chunks) and adding movement back on android

--- src/engine/events.d
+++ src/engine/events.d
@@ -82,7 +82,7 @@ Intersection[] getHits(ref App app, float[3][2] ray, bool showRay = true){
     app.objects[x].window = false;
     if (intersections.any!(i => i.intersects)) {
       app.objects[x].box.setColor(Colors.paleturquoise);
-      app.gui.showObjects = true;
+      version(Android) {} else { app.gui.showObjects = true; }
       hits ~= intersections;
     } else {
       app.objects[x].box.setColor();
