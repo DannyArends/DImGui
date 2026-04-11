d1388f167f05817d40f86a10c84c138ecde6935f
DannyArends <Danny.Arends@gmail.com>
2026-04-11 12:28:46 +0100

    Cleaning an comments

--- src/objects/camera.d
+++ src/objects/camera.d
@@ -17,7 +17,7 @@ struct Camera {
   alias capabilities this;
 
   float[3]        lookat        = [0.0f, 0.0f, 0.0f];     /// Position in the middle of the screen
-  float[2]        nearfar       = [0.1f, 1000.0f];         /// View distances, near [0], far [1]
+  float[2]        nearfar       = [0.1f, 500.0f];         /// View distances, near [0], far [1]
   float[3]        up            = [0.0f, 1.0f, 0.0f];     /// Defined up vector
   float           fov           = 45.0f;                  /// Field of view
   float           speed         =  0.5f;                  /// Movement speed
@@ -61,8 +61,7 @@ struct Camera {
 
 /* Create a position/rotation matrix through 3D space starting from xy */
 float[3][2] castRay(const ref Camera camera, float x, float y) nothrow {
-  float[2] ndc = [(2.0f * x) / cast(float)camera.width  - 1.0f,
-                  (2.0f * y) / cast(float)camera.height - 1.0f];
+  float[2] ndc = [(2.0f * x) / cast(float)camera.width  - 1.0f, (2.0f * y) / cast(float)camera.height - 1.0f];
   float[4] clip = [ndc[0], ndc[1], -1.0f, 1.0f];
   float[4] eye  = multiply(camera.proj().inverse(), clip);
   float[3] dir  = multiply(camera.view.inverse(), [eye[0], eye[1], eye[2], 0.0f]).xyz;
@@ -78,12 +77,11 @@ float[3][2] castRay(const ref Camera camera, float x, float y) nothrow {
   if(camera.rotation[0]  > 360) camera.rotation[0] = 0;
   if(camera.rotation[0]  < 0) camera.rotation[0] = 360;
 
-  camera.rotation[1] -= yrel;
-  if(camera.rotation[1]  > 65) camera.rotation[1] = 65;
-  if(camera.rotation[1]  < -65) camera.rotation[1] = -65;
-
+  camera.rotation[1] = clamp(camera.rotation[1] -= yrel, -65.0f, 65.0f);
   camera.isDirty = true;
 }
+
+/* Zoom the distance of the camera to the position the camera looks at */
 @nogc void zoom(ref Camera camera, float delta) nothrow {
   camera.distance = clamp(camera.distance + delta, 2.0f, 60.0f);
   camera.isDirty = true;
