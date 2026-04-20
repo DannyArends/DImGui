232cb35a70e2197070f2196829c930344e8ade96
DannyArends <Danny.Arends@gmail.com>
2026-04-20 09:49:55 +0100

    Minor updates to claimBestJob

--- src/engine/imgui/dwarfwindow.d
+++ src/engine/imgui/dwarfwindow.d
@@ -4,23 +4,32 @@
  */
 
 import engine;
-import dwarf : Dwarf, spawnDwarf, randomDwarfName;
+import dwarf : Dwarf, miningQueue, spawnDwarf, randomDwarfName;
 import imgui : faIcon, iconText;
 
 void showDwarfContent(ref App app, uint font = 0) {
-  // Spawn new dwarf
   igText("Spawn Dwarf:");
   igSameLine(0, 5);
   if(igButton(iconText(cast(string)ICON_FA_PLUS, "Spawn"), ImVec2(0,0))) { app.spawnDwarf(randomDwarfName()); }
 
   igSeparator();
+  int claimed = 0;
+  int mining  = 0;
+  foreach(o; app.objects) {
+    auto d = cast(Dwarf)o;
+    if(d is null) continue;
+    if(d.targetTile[0] != int.min) {
+      if(d.path.length == 0) mining++;
+      else claimed++;
+    }
+  }
+  igText(toStringz(format("Queue: %d jobs | Claimed: %d | Mining: %d", miningQueue.length, claimed, mining)));
 
-  // List existing dwarfs
+  igSeparator();
   foreach(o; app.objects) {
     auto d = cast(Dwarf)o;
     if(d is null) continue;
     igText(toStringz(format("%s %s @ [%d,%d,%d]", fromStringz(faIcon(cast(string)ICON_FA_USER)), d.dwarfName, d.tilePos[0], d.tilePos[1], d.tilePos[2])));
   }
-
 }
 
