167281e2aad20cd0d2f51039257c1dcd43c99c4a
DannyArends <Danny.Arends@gmail.com>
2026-04-23 16:32:56 +0100

    Minor changes

--- src/engine/tileatlas.d
+++ src/engine/tileatlas.d
@@ -16,8 +16,8 @@ struct TileT {
 }
 
 enum TileType : ubyte {
-  None, Lava, Water, Path08, Sand01, Sand02, Sand03, Sand05, Gravel, Moss01, Ground08, 
-  Grass01, Grass02, Grass03, Grass04, Forest01, Forest02, Stone01, Stone05, Ice01, Snow, Wood, Leaves
+  None, Lava, Water, Sand01, Sand02, Sand03, Sand05, Gravel, Moss01, Ground08, 
+  Grass01, Grass02, Grass03, Grass04, Forest01, Forest02, Stone01, Stone02, Stone03, Stone05, Ice01, Snow, Wood, Leaves
 }
 
 immutable TileT[TileType] tileData;
@@ -26,7 +26,6 @@ shared static this() {
     TileType.None: TileT("None", false, 0.0f),
     TileType.Lava: TileT("Lava_01", false, 0.0f),
     TileType.Water: TileT("Water_01", false, 0.0f),
-    TileType.Path08: TileT("Path_08", true,  1.5f),
     TileType.Sand01: TileT("Sand_01", true,  1.5f),
     TileType.Sand02: TileT("Sand_02", true,  1.5f),
     TileType.Sand03: TileT("Sand_03", true,  1.5f),
@@ -41,6 +40,8 @@ shared static this() {
     TileType.Forest01: TileT("Forest_Ground_01", true,  1.5f),
     TileType.Forest02: TileT("Jungle_01", true,  1.5f),
     TileType.Stone01: TileT("Stone_01", true,  1.8f),
+    TileType.Stone02: TileT("Stone_02", true,  1.8f),
+    TileType.Stone03: TileT("Stone_03", true,  1.8f),
     TileType.Stone05: TileT("Stone_05", true,  1.5f),
     TileType.Ice01: TileT("Ice_01", true,  1.0f),
     TileType.Snow: TileT("Ice_03", true,  1.6f),
@@ -53,9 +54,9 @@ shared static this() {
   if (h < 0.05f) return TileType.Lava;
   if (h < 0.15f){ TileType[3] variants = [TileType.Stone01, TileType.Gravel, TileType.Moss01]; return variants[cast(uint)(t * 3) % 3]; }
   if (h < 0.25f){ TileType[4] variants = [TileType.Sand01, TileType.Sand02, TileType.Sand03, TileType.Sand05]; return variants[cast(uint)(t * 4) % 4]; }
-  if (h < 0.35f){ TileType[4] variants = [TileType.Forest02, TileType.Sand02, TileType.Path08, TileType.Grass02]; return variants[cast(uint)(t * 4) % 4]; }
+  if (h < 0.35f){ TileType[4] variants = [TileType.Forest02, TileType.Sand02, TileType.Forest01, TileType.Grass02]; return variants[cast(uint)(t * 4) % 4]; }
   if (h < 0.50f){ TileType[4] variants = [TileType.Grass01, TileType.Grass02, TileType.Grass01, TileType.Grass04]; return variants[cast(uint)(t * 4) % 4]; }
-  if (h < 0.70f){ TileType[4] variants = [TileType.Grass04, TileType.Grass01, TileType.Forest01, TileType.Forest02]; return variants[cast(uint)(t * 4) % 4]; }
+  if (h < 0.70f){ TileType[4] variants = [TileType.Grass04, TileType.Grass01, TileType.Stone02, TileType.Forest02]; return variants[cast(uint)(t * 4) % 4]; }
   if (h < 0.80f){ TileType[3] variants = [TileType.Stone01, TileType.Stone05, TileType.Forest01]; return variants[cast(uint)(t * 3) % 3]; }
   if (h < 0.85f) return TileType.Stone01;
   if (h < 0.90f) return TileType.Ice01;
