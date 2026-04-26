04482469d36f7625062fbb3f158f00b210917ec5
DannyArends <Danny.Arends@gmail.com>
2026-04-26 18:39:52 +0100

    Remorked Dwarf and Inventory window

--- src/engine/imgui/dwarfwindow.d
+++ src/engine/imgui/dwarfwindow.d
@@ -8,6 +8,20 @@ import engine;
 import dwarf : spawnDwarf, randomDwarfName;
 import jobs : jobQueue;
 import imgui : faIcon, iconText;
+import textures : ImTextureRefFromID, idx;
+import tileatlas : tileData;
+
+void showTileIcons(ref App app, TileType[] tiles, float cellSize = 16.0f) {
+  foreach(tt; tiles.sort.uniq) {
+    igSameLine(0, 2);
+    auto name = tileData[tt].name;
+    auto texIdx = idx(app.textures, name ~ "_base");
+    if(texIdx < 0) continue;
+    auto texID = ImTextureRefFromID(cast(ulong)app.textures[texIdx].imID);
+    igImage(texID, ImVec2(cellSize, cellSize), ImVec2(0,0), ImVec2(1,1));
+    if(igIsItemHovered(0)) igSetTooltip(toStringz(format("%s x%d", name, tiles.count(tt))));
+  }
+}
 
 void showDwarfContent(ref App app, uint font = 0) {
   igText("Spawn Dwarf:");
@@ -25,11 +39,13 @@ void showDwarfContent(ref App app, uint font = 0) {
     else if(d.isWandering) { status = "Wandering"; }
     else if(d.path.length > 0) { status = format("Walking -> %s", d.jobStack[0].name); walking++; }
     else { status = d.jobStack[0].name; working++; }
-    if(d.carrying.length > 0) status ~= format(" [carrying: %s]", d.carrying);
-    igText(toStringz("%s"), toStringz(format("%s %s @ %s - %s", fromStringz(faIcon(cast(string)ICON_FA_USER)), d.name, d.tile, status)));
+    igText(toStringz("%s"), toStringz(format("%s %s", fromStringz(faIcon(cast(string)ICON_FA_USER)), d.name)));
+    if(d.carrying.length > 0) {
+      igSameLine(0, 5);
+      app.showTileIcons(d.carrying);
+    }
+    igText(toStringz("%s"), toStringz(format("%s - %s\n", d.tile, status)));
   }
-
-  igSeparator();
   igText(toStringz(format("Queue: %d | Idle: %d | Walking: %d | Working: %d", jobQueue.length, idle, walking, working)));
 
   igSeparator();
