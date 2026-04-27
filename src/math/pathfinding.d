424a7132e02233130f8a2f9e9ed40025bcd83247
DannyArends <Danny.Arends@gmail.com>
2026-04-27 16:05:00 +0100

    ref T

--- src/math/pathfinding.d
+++ src/math/pathfinding.d
@@ -9,7 +9,7 @@ import search : performSearch, atGoal, stepThroughPath;
 
 /** Pathfind object T to goalTile, returns false if unreachable.
  * Requires T to have: tile, path */
-bool pathfindTo(T)(ref App app, T obj, int[3] goalTile) {
+bool pathfindTo(T)(ref App app, ref T obj, int[3] goalTile) {
   float[3] start = app.world.tileToWorld(obj.tile);
   float[3] goal  = app.world.tileToWorld(goalTile);
   if(app.verbose) SDL_Log(toStringz(format("pathfindTo: %s -> %s", start, goal)));
@@ -24,7 +24,7 @@ bool pathfindTo(T)(ref App app, T obj, int[3] goalTile) {
 
 /** Check if object T is adjacent to targetTile.
  * Requires T to have: tile */
-bool atDestination(T)(ref App app, T obj, int[3] targetTile) {
+bool atDestination(T)(ref App app, ref T obj, int[3] targetTile) {
   auto dx = abs(obj.tile[0] - targetTile[0]);
   auto dz = abs(obj.tile[2] - targetTile[2]);
   return dx + dz == 1 && obj.tile[1] == targetTile[1];
@@ -32,7 +32,7 @@ bool atDestination(T)(ref App app, T obj, int[3] targetTile) {
 
 /** Attempt to re-path object T to goalTile, returns false if unreachable.
  * Requires T to have: tile, targetTile, path, visualPos, moveFrom, moveTo, moveT */
-bool repathTo(T)(ref App app, T obj, int[3] targetTile) {
+bool repathTo(T)(ref App app, ref T obj, int[3] targetTile) {
   obj.targetTile = targetTile;
   auto goalTile = app.findGoalTile(obj);
   if(goalTile[0] == int.min) return false;
@@ -45,7 +45,7 @@ bool repathTo(T)(ref App app, T obj, int[3] targetTile) {
 
 /** Find the closest standable neighbour (air tile with solid below) to the object.
  * Requires T to have: tile, targetTile */
-int[3] findGoalTile(T)(ref App app, T obj) {
+int[3] findGoalTile(T)(ref App app, ref T obj) {
   int[3] goalTile = [int.min, 0, 0];
   float bestDist = float.max;
   foreach(n; app.world.tileNeighbours(obj.targetTile)[0..2] ~ app.world.tileNeighbours(obj.targetTile)[4..6]) {
@@ -58,7 +58,7 @@ int[3] findGoalTile(T)(ref App app, T obj) {
 
 /** Follow the next step in object T's path.
  * Requires T to have: tile, path, visualPos, moveFrom, moveTo, moveT */
-void followPath(T)(ref App app, T obj) {
+void followPath(T)(ref App app, ref T obj) {
   if(obj.path.length == 0) return;
   auto next = obj.path[0];
   obj.path = obj.path[1..$];
