e57506e7f2a3203c3f223db1d465a5dcfcb6eb23
DannyArends <Danny.Arends@gmail.com>
2026-04-29 19:20:55 +0100

    Minor changes to post processing

--- app/src/main/assets/data/shaders/postvertex.glsl
+++ app/src/main/assets/data/shaders/postvertex.glsl
@@ -1,13 +1,17 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
-// Generates a fullscreen triangle that covers the entire screen.
+layout(location = 0) out vec2 fragTexCoord;
+
 void main() {
   if (gl_VertexIndex == 0) {
     gl_Position = vec4(-1.0, 3.0, 0.0, 1.0);
+    fragTexCoord = vec2(0.0, 2.0);
   } else if (gl_VertexIndex == 1) {
     gl_Position = vec4(-1.0, -1.0, 0.0, 1.0);
-  } else { // gl_VertexIndex == 2
+    fragTexCoord = vec2(0.0, 0.0);
+  } else {
     gl_Position = vec4(3.0, -1.0, 0.0, 1.0);
+    fragTexCoord = vec2(2.0, 0.0);
   }
 }
