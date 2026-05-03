da03622e539ab49365f3ad81d639412a3b98f86a
DannyArends <Danny.Arends@gmail.com>
2026-05-03 12:03:36 +0100

    Minimize

--- src/objects/geometry.d
+++ src/objects/geometry.d
@@ -12,23 +12,21 @@ import textures : idx;
 import mesh : logMesh;
 import vector : vSub, vAdd, dot, vMul, cross, normalize, euclidean;
 
-/** An instance of a Geometry
- */
+/** An instance of a Geometry */
 struct Instance {
   uint[4] meshdef = [0, 0, 0, 0];               /// Mesh Definition [start, end, mID, pad]
   Matrix matrix = Matrix.init;                  /// Instance matrix
   alias matrix this;
 
   this(uint[4] d) { meshdef = d; }
-  this(TileType tt, Matrix m) { meshdef[0..2] = [cast(uint)tt, cast(uint)tt]; matrix = m; }
+  this(TileType tt, Matrix m) { this([cast(uint)tt, cast(uint)tt], m); }
   this(uint[2] d, Matrix m) { meshdef[0..2] = d; matrix = m; }
   this(uint mdef, float[12] f) { this([mdef, mdef], Matrix([f[0],f[1],f[2], 0, f[3],f[4],f[5], 0, f[6],f[7],f[8],0, f[9],f[10],f[11], 1])); }
 }
 
 shared uint guid = 1;
 
-/** A Geometry that can be rendered
- */
+/** A Geometry that can be rendered */
 class Geometry {
   GeometryBuffer vertexBuffer;
   GeometryBuffer indexBuffer;
@@ -152,8 +150,7 @@ float scale(T)(T object, uint instance = 0) {
   return(scale(object.instances[instance]));
 }
 
-/** Set different types of textures on an object
- */
+/** Set different types of textures on an object */
 void setTexture(T)(T object, string name, aiTextureType tt) {
   if(object.materials.length == 0) {
     object.materials.length = 1;
