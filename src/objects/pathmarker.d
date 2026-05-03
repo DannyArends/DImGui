4a0a3bd97493811132862f808515d16112aaaf76
DannyArends <Danny.Arends@gmail.com>
2026-05-03 15:33:52 +0100

    Initial path markers

---
+++ src/objects/pathmarker.d
@@ -0,0 +1,29 @@
+/** 
+ * Authors: Danny Arends
+ * License: GPL-v3 (See accompanying file LICENSE.txt or copy at https://www.gnu.org/licenses/gpl-3.0.en.html)
+ */
+
+import engine;
+
+import matrix : translateScale;
+
+class PathMarkers : Cylinder {
+  this() {
+    super(0.1f, 0.2f, 6);
+    initInstanced(() => "PathMarkers");
+  }
+}
+
+/** Rebuild path marker instances from all dwarf paths */
+void syncPathMarkers(ref App app) {
+  if(app.world.pathMarkers is null || app.world.dwarves is null) return;
+  app.world.pathMarkers.instances = [];
+  if(app.showPaths) {
+    foreach(ref d; app.world.dwarves) {
+      foreach(ref wp; d.path) {
+        app.world.pathMarkers.instances ~= DrawInstance([0, 0, d.colorID, 0], translateScale(wp, [0.2f, 0.2f, 0.2f]));
+      }
+    }
+  }
+  app.world.pathMarkers.markDirty();
+}
