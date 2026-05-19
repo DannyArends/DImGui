93fc35f6214c82c0f6e4f39b95d6aee3dac04f20
DannyArends <Danny.Arends@gmail.com>
2026-05-19 16:47:02 +0100

    Cleanup

--- src/engine/material.d
+++ src/engine/material.d
@@ -7,27 +7,18 @@ import engine;
 
 import textures : idx;
 
-shared uint muid = 0;
-
 struct Material {
   uint mid;
   int tid = -1;
   int nid = -1;
   int oid = -1;
-
-  this(int tid, int nid, int oid) {
-    this.mid = atomicOp!"+="(muid, 1) - 1;
-    this.tid = tid;
-    this.nid = nid;
-    this.oid = oid;
-  }
 }
 
 void registerAMaterials(ref App app, ref Geometry object) {
   foreach(ref mesh; object.meshes) {
     if(mesh.mid < 0 || mesh.mid >= object.materials.length) continue;
     mesh.mat = mesh.mid;  // save local index before remap
-    app.materials ~= Material(-1, -1, -1);
+    app.materials ~= Material(cast(uint)app.materials.length, -1, -1, -1);
     mesh.mid = app.materials[$-1].mid;
   }
 }
--- src/game/resources.d
+++ src/game/resources.d
@@ -22,8 +22,7 @@ struct ResourceT {
 
 void injectResourceMeshes(ref App app) {
   foreach (tt; 0 .. cast(int)ResourceType.max + 1) {
-    auto ttype = cast(ResourceType)tt;
-    app.materials ~= Material(-1, -1, -1);
+    app.materials ~= Material(cast(uint)app.materials.length, -1, -1, -1);
     app.meshes ~= Mesh([0, 0], app.materials[$-1].mid);
   }
 }
