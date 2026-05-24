9e6f309e1d92656e983cf6785ae9e440aa34e381
DannyArends <Danny.Arends@gmail.com>
2026-05-24 11:27:00 +0100

    Updates to materials

--- src/engine/material.d
+++ src/engine/material.d
@@ -23,6 +23,15 @@ void registerAMaterials(ref App app, ref Geometry object) {
   }
 }
 
+void ensureMaterial(ref App app, ref Geometry object) {
+  foreach(ref mesh; object.meshes) {
+    if(mesh.mid >= 0) continue;
+    mesh.mid = cast(int)(app.materials.length);
+    app.materials ~= Material();
+  }
+  app.buffers["MaterialBuffer"].dirty[] = true;
+}
+
 void updateMaterials(ref App app) {
   foreach (tt; 0 .. cast(int)ResourceType.max + 1) {
     auto ttype = cast(ResourceType)tt;
