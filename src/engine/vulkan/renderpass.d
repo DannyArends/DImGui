c659b850fcebf4d8e8b08ac343cae2b7724d1ae0
DannyArends <Danny.Arends@gmail.com>
2026-05-25 13:45:58 +0100

    colorFormat to offscreenFormat

--- src/engine/vulkan/renderpass.d
+++ src/engine/vulkan/renderpass.d
@@ -70,16 +70,16 @@ void createSceneRenderPass(ref App app) {
 
   RenderPassInfo info = {
     attachments: [
-      { format: app.colorFormat,       samples: app.getMSAASamples(),  loadOp: VK_ATTACHMENT_LOAD_OP_CLEAR,
+      { format: app.offscreenFormat, samples: app.getMSAASamples(), loadOp: VK_ATTACHMENT_LOAD_OP_CLEAR,
         storeOp: VK_ATTACHMENT_STORE_OP_DONT_CARE,
         initialLayout: VK_IMAGE_LAYOUT_UNDEFINED,
         finalLayout:   VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL },
-      { format: app.colorFormat,       samples: VK_SAMPLE_COUNT_1_BIT, loadOp: VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+      { format: app.offscreenFormat, samples: VK_SAMPLE_COUNT_1_BIT, loadOp: VK_ATTACHMENT_LOAD_OP_DONT_CARE,
         storeOp: VK_ATTACHMENT_STORE_OP_STORE,
         stencilLoadOp: VK_ATTACHMENT_LOAD_OP_DONT_CARE, stencilStoreOp: VK_ATTACHMENT_STORE_OP_DONT_CARE,
         initialLayout: VK_IMAGE_LAYOUT_UNDEFINED,
         finalLayout:   VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL },
-      { format: app.findDepthFormat(), samples: app.getMSAASamples(),  loadOp: VK_ATTACHMENT_LOAD_OP_CLEAR,
+      { format: app.findDepthFormat(), samples: app.getMSAASamples(), loadOp: VK_ATTACHMENT_LOAD_OP_CLEAR,
         storeOp: VK_ATTACHMENT_STORE_OP_STORE,
         initialLayout: VK_IMAGE_LAYOUT_UNDEFINED,
         finalLayout:   VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL },
