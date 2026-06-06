1553a6b907dcb3b6a56fabfd4b10902718ed8e13
DannyArends <Danny.Arends@gmail.com>
2026-06-06 17:46:39 +0100

    Hmm, some debug

--- src/engine/shadow.d
+++ src/engine/shadow.d
@@ -157,7 +157,7 @@ void createShadowMapGraphicsPipeline(ref App app) {
     depthClampEnable: VK_FALSE,
     polygonMode: VK_POLYGON_MODE_FILL,
     lineWidth: 1.0f,
-    cullMode: VK_CULL_MODE_FRONT_BIT,
+    cullMode: VK_CULL_MODE_NONE,
     frontFace: VK_FRONT_FACE_COUNTER_CLOCKWISE,
     depthBiasEnable: VK_TRUE,
     depthBiasConstantFactor: 1.0f,
