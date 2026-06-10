aa3422da674e9beaaaa8de5cf307db88c284e851
DannyArends <Danny.Arends@gmail.com>
2026-06-10 12:06:34 +0100

    Current state

--- app/src/main/assets/data/shaders/scene.glsl
+++ app/src/main/assets/data/shaders/scene.glsl
@@ -12,24 +12,24 @@ layout(constant_id = 1) const bool ALPHA_TEST = true;
 layout(constant_id = 2) const bool INSTANCED = true;
 
 /// Uniform Buffer Objects
-#define BINDING_SCENE_UBO         0
-#define BINDING_LIGHT_UBO         1
+#define BINDING_SCENE_UBO          0
+#define BINDING_LIGHT_UBO          1
 
 /// Shader Storage Buffer Objects
-#define BINDING_BONES_SSBO        2
-#define BINDING_MESH_SSBO         3
-#define BINDING_LIGHT_SSBO        4
+#define BINDING_BONES_SSBO         2
+#define BINDING_MESH_SSBO          3
+#define BINDING_LIGHT_SSBO         4
 
 /// Samplers/Images (defined in samplers.glsl)
-// #define BINDING_TEXTURES          5
-// #define BINDING_SHADOWMAP         6
+//BINDING_TEXTURES = 5 & BINDING_SHADOWMAP = 6
 
 /// Materials
-#define BINDING_MATERIAL_SSBO     7
+#define BINDING_MATERIAL_SSBO      7
 
 /// Lights
-#define BINDING_CLUSTER_LIGHTS    8
-#define BINDING_CLUSTER_RANGE     9
+#define BINDING_CLUSTER_LIGHTS     8
+#define BINDING_CLUSTER_RANGE      9
+#define BINDING_CLUSTER_COUNTER   10
 
 struct Light {
   mat4 lightProjView; /// Combined light's projection * light's view matrix
@@ -58,32 +58,37 @@ struct Material {
 };
 
 struct LightIndex { uint light; };
+struct Cursor { uint cursor; };
 struct ClusterRange { uint offset; uint count; };
 
 /// Shader Storage Buffer Objects
 layout (std430, set = 0, binding = BINDING_BONES_SSBO) readonly buffer BoneMatrices {
-    Bone transforms[];
+  Bone transforms[];
 } boneSSBO;       // 2
 
 layout (std430, set = 0, binding = BINDING_MESH_SSBO) readonly buffer MeshMatrices {
-    Mesh meshes[];
+  Mesh meshes[];
 } meshSSBO;       // 3
 
 layout (std430, set = 0, binding = BINDING_LIGHT_SSBO) readonly buffer LightMatrices {
-    Light lights[];
+  Light lights[];
 } lightSSBO;      // 4
 
 layout (std430, set = 0, binding = BINDING_MATERIAL_SSBO) readonly buffer MaterialBuffer {
-    Material materials[];
+  Material materials[];
 } materialSSBO;   // 7
 
-layout(std430, set=0, binding=BINDING_CLUSTER_LIGHTS) buffer ClusterLights { 
+layout(std430, set=0, binding=BINDING_CLUSTER_LIGHTS) buffer ClusterLights {
   LightIndex indices[];
 }; // 8
 
-layout(std430, set=0, binding=BINDING_CLUSTER_RANGE) buffer ClusterRanges { 
+layout(std430, set=0, binding=BINDING_CLUSTER_RANGE) buffer ClusterRanges {
   ClusterRange ranges[];
 }; // 9
+
+layout(std430, set=0, binding=BINDING_CLUSTER_COUNTER) buffer ClusterCounter {
+  Cursor cursor[];
+};
 
 /// UBO
 layout(std140, binding = BINDING_SCENE_UBO) uniform UniformBufferObject {
@@ -91,6 +96,7 @@ layout(std140, binding = BINDING_SCENE_UBO) uniform UniformBufferObject {
   mat4 scene;                 /// Scene Camera adjustment
   mat4 view;                  /// View matrix
   mat4 proj;                  /// Projection matrix
+  mat4 invProj;               /// Inverse projection matrix
   mat4 ori;                   /// Screen orientation
   float shadowTexelSize;      /// Shadow texel size
   uint nlights;               /// Number of actual lights
