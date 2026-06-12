cfe04d0d733ff674df3fcaf5a8e130d1c5227176
DannyArends <Danny.Arends@gmail.com>
2026-06-12 18:09:28 +0100

    Moving torches

--- src/game/dwarf.d
+++ src/game/dwarf.d
@@ -270,9 +270,9 @@ void addDwarf(ref GameApp app, ref Dwarf d) {
   DrawInstance inst = DrawInstance([0, 0], d.color, Matrix.init);
   inst = position(inst, d.visualPos);
   app.world.dwarves.instances ~= inst;
-  app.world.dwarves ~= d;
   app.addLight(torchLight(d.visualPos));
   d.lightIndex = app.lights.length - 1;
+  app.world.dwarves ~= d;
 }
 
 /** Spawn a Dwarf */
