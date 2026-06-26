9f4879f70d430c1414a4451ac4641f38adec396c
DannyArends <Danny.Arends@gmail.com>
2026-06-26 07:03:43 +0100

    Bounding-box logic

--- src/engine/vulkan/commands.d
+++ src/engine/vulkan/commands.d
@@ -41,7 +41,7 @@ void drawBoundingBoxes(ref App app, VkCommandBuffer cmd) {
 
   vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, app.pipelines[VK_PRIMITIVE_TOPOLOGY_LINE_LIST].pipeline(Specialization(false, true)));
   for(size_t x = 0; x < app.objects.length; x++) {
-    if(!app.objects[x].isBuffered || !app.objects[x].inFrustum || !app.objects[x].isVisible) continue; // not Buffered, not in Frustum, not Visible
+    if(!app.objects[x].isDrawable || !app.objects[x].inFrustum || !app.objects[x].isVisible) continue; // not Drawable, not in Frustum, not Visible
     if(app.objects[x].hasBoundingBox && app.objects[x].box.isDrawable) app.draw(app.objects[x].box, cmd);
   }
   popLabel(cmd);
