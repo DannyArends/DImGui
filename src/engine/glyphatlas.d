1c22e801c77ccf4903ad1b6d8d628f1be9c5b89d
DannyArends <Danny.Arends@gmail.com>
2026-07-04 14:18:30 +0100

    Fixes

--- src/engine/glyphatlas.d
+++ src/engine/glyphatlas.d
@@ -22,7 +22,7 @@ struct Glyph {
   int maxy;
   int advance;
   int atlasloc;
-  int atlasrow;
+  int atlasY;   // actual pixel Y where this glyph was blitted
 
   @property @nogc int gX() nothrow { return(advance - minx); }
   @property @nogc int gY() nothrow { return(maxy - miny); }
@@ -34,7 +34,6 @@ struct GlyphAtlas {
   TTF_Font* ttf;        /// Pointer to the loaded TTF_Font
   ubyte pointsize;      /// Font pointsize size
   int lineHeight;       /// Full line height (ascent + descent)
-  int rowPitch;         /// Atlas row height = lineHeight + 2*SDF spread (uniform, observed once)
   Glyph[dchar] glyphs;  /// Associative array couples Glyph and dchar
   string atlas;         /// UTF-8 string of chars stored in the atlas
   Texture texture;      /// Holds the Texture structure containing the SDL surface and Vulkan buffers
@@ -54,7 +53,7 @@ struct GlyphAtlas {
   }
 
   /** Glyph texture Y postion */
-  @property float tY(Glyph glyph) { return((this.rowPitch * glyph.atlasrow) / cast(float)(texture.height)); }
+  @property float tY(Glyph glyph) { return(glyph.atlasY / cast(float)(texture.height)); }
 
   /** X postion of the glyph, when on column col */
   @property float pX(Glyph glyph, size_t col) {
@@ -63,7 +62,7 @@ struct GlyphAtlas {
 
   /** Y postion of the glyph, when on line[0] out of line[1] */
   @property float pY(Glyph glyph, size_t[2] line) {
-    return(cast(float)(line[1] - line[0]) * this.rowPitch + (this.ascent - glyph.maxy));
+    return(cast(float)(line[1] - line[0]) * this.lineHeight + (this.ascent - glyph.maxy));
   }
 
   alias texture this;
@@ -95,7 +94,8 @@ void createGlyphAtlas(ref App app, dchar to = '\U00000FFF', uint dim = 1024) {
   SDL_SetSurfaceBlendMode(app.glyphAtlas.surface, SDL_BLENDMODE_NONE);
   app.glyphAtlas.width = app.glyphAtlas.height = dim;
 
-  uint i, atlasrow, atlasloc = 0;
+  uint i, atlasloc = 0;
+  int penY = 0, rowMaxH = 0;
   app.glyphAtlas.atlas = [];
   dchar c = '\U00000000';
   while (c <= to) {
@@ -104,21 +104,20 @@ void createGlyphAtlas(ref App app, dchar to = '\U00000FFF', uint dim = 1024) {
       TTF_GetGlyphMetrics(app.glyphAtlas.ttf, cast(uint)(c), &glyph.minx, &glyph.maxx, &glyph.miny, &glyph.maxy, &glyph.advance);
       auto gs = TTF_RenderGlyph_Blended(app.glyphAtlas.ttf, cast(uint)(c), SDL_Color(255, 255, 255, 255));
       if (!gs) { c++; continue; }
-      if (app.glyphAtlas.rowPitch == 0) app.glyphAtlas.rowPitch = app.glyphAtlas.lineHeight + (gs.w - (glyph.advance - glyph.minx));
-      if (atlasloc + gs.w >= app.glyphAtlas.width) { i = atlasloc = 0; atlasrow++; }
-      if (atlasrow * app.glyphAtlas.rowPitch + app.glyphAtlas.rowPitch > app.glyphAtlas.height) {
+      if (atlasloc + gs.w >= app.glyphAtlas.width) { i = atlasloc = 0; penY += rowMaxH; rowMaxH = 0; }
+      if (penY + gs.h > app.glyphAtlas.height) {
         SDL_DestroySurface(gs);
         SDL_Log("WARNING: GlyphAtlas overflow at (and after) character %d", c);
         break;
       }
+      if (gs.h > rowMaxH) rowMaxH = gs.h;
       if (app.glyphAtlas.advance < glyph.advance) app.glyphAtlas.advance = glyph.advance;
       if (app.glyphAtlas.miny > glyph.miny) app.glyphAtlas.miny = glyph.miny;
-      glyph.atlasloc = atlasloc; glyph.atlasrow = atlasrow;
+      glyph.atlasloc = atlasloc; glyph.atlasY = penY;
       glyph.w = gs.w; glyph.h = gs.h;
-      SDL_Log(cstr("%c %s", c, glyph));
       app.glyphAtlas.glyphs[c] = glyph;
       app.glyphAtlas.atlas ~= c;
-      SDL_Rect dst = { atlasloc, atlasrow * app.glyphAtlas.rowPitch, gs.w, gs.h };
+      SDL_Rect dst = { atlasloc, penY, gs.w, gs.h };
       SDL_BlitSurface(gs, null, app.glyphAtlas.surface, &dst);
       atlasloc += gs.w;
       SDL_DestroySurface(gs);
@@ -131,7 +130,7 @@ void createGlyphAtlas(ref App app, dchar to = '\U00000FFF', uint dim = 1024) {
     SDL_Log("%d unicode glyphs (%d unique ones)", app.glyphAtlas.atlas.length, app.glyphAtlas.glyphs.length);
     SDL_Log("FontAscent: %d, FontAdvance: %d", app.glyphAtlas.ascent, app.glyphAtlas.advance);
   }
-  SDL_Log("%d/%d Glyphs on %d lines [%d x %d] in %d msecs\n", app.glyphAtlas.glyphs.length, c, ++atlasrow, dim, dim, time);
+  SDL_Log("%d/%d Glyphs [%d x %d] in %d msecs\n", app.glyphAtlas.glyphs.length, c, dim, dim, time);
 }
 
 /** Create a TextureImage layout and view from the SDL_Surface and adds it to the App.textureArray */
