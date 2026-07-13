d6d3d005615b07d1368688c5b3df0dbc572ab09d
DannyArends <Danny.Arends@gmail.com>
2026-07-13 16:49:12 +0100

    How ?

--- src/engine/vulkan/instancing.d
+++ src/engine/vulkan/instancing.d
@@ -7,7 +7,8 @@ import engine;
 
 /** An instance of a Geometry */
 struct DrawInstance {
-  int[4] meshdef = [0, 0, -1, 0];               /// Mesh range [start, end, material, unused] in meshSSBO; material -1 = use mesh material
+  int[3] meshdef = [0, 0, -1];                  /// Mesh range [start, end, material, unused] in meshSSBO; material -1 = use mesh material
+  int pad = 0;                                  /// Pad ?
   float[4] color = [1.0f, 1.0f, 1.0f, 1.0f];    /// Color
   float[4] uvRect = [0.0f, 0.0f, 1.0f, 1.0f];   /// UV remap [offsetX, offsetY, scaleX, scaleY]; identity = full texture
   Matrix matrix = Matrix.init;                  /// Matrix
@@ -18,8 +19,8 @@ struct DrawInstance {
   static assert(DrawInstance.matrix.offsetof == 48);
 
   this(int mat, Matrix m) { meshdef[2] = mat; matrix = m; }
-  this(uint[2] d, Matrix m = Matrix.init) { meshdef[0..2] = [cast(int)d[0], cast(int)d[1]]; matrix = m; }
-  this(uint[2] d, float[4] c, Matrix m = Matrix.init) { meshdef[0..2] = [cast(int)d[0], cast(int)d[1]]; color = c; matrix = m; }
+  this(int[2] d, Matrix m = Matrix.init) { meshdef[0..2] = d[0..2]; matrix = m; }
+  this(int[2] d, float[4] c, Matrix m = Matrix.init) { meshdef[0..2] = d[0..2]; color = c; matrix = m; }
   this(int mat, float[4] c, Matrix m) { meshdef[2] = mat; color = c; matrix = m; }
   this(int mdef, float[12] f) { 
     this([cast(uint)mdef, cast(uint)mdef], Matrix([f[0],f[1],f[2],0, f[3],f[4],f[5],0, f[6],f[7],f[8],0, f[9],f[10],f[11],1])); 
--- src/engine/vulkan/vertex.d
+++ src/engine/vulkan/vertex.d
@@ -36,7 +36,7 @@ struct Vertex {
       { binding: VERTEX, location: 5, format: VK_FORMAT_R32G32B32A32_UINT, offset: Vertex.bones.offsetof },
       { binding: VERTEX, location: 6, format: VK_FORMAT_R32G32B32A32_SFLOAT, offset: Vertex.weights.offsetof },
 
-      { binding: INSTANCE, location: 7, format: VK_FORMAT_R32G32B32A32_SINT, offset: DrawInstance.meshdef.offsetof },
+      { binding: INSTANCE, location: 7, format: VK_FORMAT_R32G32B32_SINT, offset: DrawInstance.meshdef.offsetof },
       { binding: INSTANCE, location: 8, format: VK_FORMAT_R32G32B32A32_SFLOAT, offset: DrawInstance.color.offsetof },
       { binding: INSTANCE, location: 9, format: VK_FORMAT_R32G32B32A32_SFLOAT, offset: DrawInstance.uvRect.offsetof },
       { binding: INSTANCE, location: 10, format: VK_FORMAT_R32G32B32A32_SFLOAT, offset: DrawInstance.matrix.offsetof },
