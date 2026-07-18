e2597b59e8490a89e0df30ed7128327def4bce5a
DannyArends <Danny.Arends@gmail.com>
2026-07-18 06:58:01 +0100

    Minor tweaks for Android

--- src/engine/vulkan/devices.d
+++ src/engine/vulkan/devices.d
@@ -24,7 +24,7 @@ void pickPhysicalDevice(ref App app, uint device = 0){
 }
 
 VkSampleCountFlagBits getMSAASamples(ref App app) {
-  version (Android) { return VK_SAMPLE_COUNT_2_BIT; }
+  version (Android) { return VK_SAMPLE_COUNT_4_BIT; }
   VkSampleCountFlags counts = app.properties.limits.framebufferColorSampleCounts & app.properties.limits.framebufferDepthSampleCounts;
   if (counts & VK_SAMPLE_COUNT_64_BIT) { return VK_SAMPLE_COUNT_64_BIT; }
   if (counts & VK_SAMPLE_COUNT_32_BIT) { return VK_SAMPLE_COUNT_32_BIT; }
