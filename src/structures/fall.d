7454219f1fde0095f80df4984d88f788b82bb6ad
DannyArends <Danny.Arends@gmail.com>
2026-07-22 12:35:36 +0100

    Rename and move

--- src/structures/fall.d
+++ src/structures/fall.d
@@ -3,6 +3,8 @@
  * License: GPL-v3 (See accompanying file LICENSE.txt or copy at https://www.gnu.org/licenses/gpl-3.0.en.html)
  */
 
+import phobos;
+
 import lattice : tileToWorld;
 import vector : y;
 
