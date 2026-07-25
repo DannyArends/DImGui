c2876bb92314573ce252390521567d09ba414472
DannyArends <Danny.Arends@gmail.com>
2026-07-25 12:36:51 +0100

    Stop the swim

--- src/engine.d
+++ src/engine.d
@@ -145,7 +145,7 @@ struct App {
   // Global boolean flags
   bool finished = false;                                                        /// Is the main loop finished ?
   bool enableValidation = false;                                                /// Should validation be enabled ?
-  bool nameVulkanObjects = true;                                               /// Name Vulkan Objects via vkSetDebugUtilsObjectName
+  bool nameVulkanObjects = false;                                               /// Name Vulkan Objects via vkSetDebugUtilsObjectName
   bool showBounds = false;                                                      /// Show bounding boxes
   bool showLights = false;                                                      /// Show lights
   bool showPaths = false;                                                       /// Show pathfinding
