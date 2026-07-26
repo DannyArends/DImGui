ac61cc4cf27b7989e4d5b0c6142d6d82f34a30b3
DannyArends <Danny.Arends@gmail.com>
2026-07-26 12:33:23 +0100

    Back to 4096 on laptop

--- src/engine/shadow.d
+++ src/engine/shadow.d
@@ -31,7 +31,7 @@ struct ShadowMap {
   GraphicsPipeline pipeline;
 
   VkFormat format = VK_FORMAT_D32_SFLOAT;   /// Shadowmap format
-  uint dimension = isAndroid ? 1024 : 512; /// Shadowmap dimension
+  uint dimension = isAndroid ? 1024 : 4096; /// Shadowmap dimension
   uint budget = isAndroid ? 4 : 24;         /// Max lights casting shadows per frame (stage 1: first-K)
   float[2] bounds = [0.0f, 0.0f];           /// [height, radius] for shadow projection
 
