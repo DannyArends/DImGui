214462adc237a11d2224044966fdba28212a76fe
DannyArends <Danny.Arends@gmail.com>
2026-08-01 21:08:45 +0100

    Use it once

--- src/engine/vulkan/ssao.d
+++ src/engine/vulkan/ssao.d
@@ -9,7 +9,7 @@ import vector : normalize, vMul;
 import matrix : multiply, inverse;
 import quaternion : xyzw;
 
-enum SSAO_KERNEL = isAndroid ? 16 : 32;
+enum SSAO_KERNEL = isAndroid ? (isLowPower ? 4 : 16) : 32;
 __gshared float[4][SSAO_KERNEL] ssaoKernel;
 
 struct SSAOUniformBuffer {
