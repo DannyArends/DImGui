5d9b7bda563118290cbefd9fed58b0deece6ad23
DannyArends <Danny.Arends@gmail.com>
2026-08-02 10:39:49 +0100

    Minimal ?

--- src/structures/bounds.d
+++ src/structures/bounds.d
@@ -7,20 +7,22 @@ import phobos;
 
 import vector : x, y, z;
 
-/** Accumulates a min/max AABB. */
+/** Min/max AABB; [0] = min, [1] = max. */
 struct Bounds {
-  float[3] min = [ float.max, float.max, float.max];
-  float[3] max = [-float.max,-float.max,-float.max];
+  float[3][2] bounds = [[ float.max,  float.max,  float.max],
+                        [-float.max, -float.max, -float.max]];
+  alias bounds this;
 
   @nogc pure void update(const float[3] v) nothrow {
-    if (v.x < min[0]) min[0] = v.x;
-    if (v.y < min[1]) min[1] = v.y;
-    if (v.z < min[2]) min[2] = v.z;
-    if (v.x > max[0]) max[0] = v.x;
-    if (v.y > max[1]) max[1] = v.y;
-    if (v.z > max[2]) max[2] = v.z;
+    foreach(k; 0..3) {
+      if(v[k] < bounds[0][k]) bounds[0][k] = v[k];
+      if(v[k] > bounds[1][k]) bounds[1][k] = v[k];
+    }
   }
-  @property @nogc pure float[3] size() nothrow const { float[3] s = max[] - min[]; return(s); }
+  @nogc pure void update(const Bounds o) nothrow { update(o[0]); update(o[1]); }
+  @property @nogc pure float[3] min() nothrow const { return bounds[0]; }
+  @property @nogc pure float[3] max() nothrow const { return bounds[1]; }
+  @property @nogc pure float[3] size() nothrow const { float[3] s = bounds[1][] - bounds[0][]; return s; }
 }
 
 /** A cullable span [first, first+count) of an object's instance buffer. */
@@ -30,11 +32,11 @@ struct DrawRange {
   bool visible = true;   /// Cached frustum result
 
   /** Union this range's per-instance world-AABBs into one AABB. */
-  @nogc float[3][2] bounds(const float[3][2][] world) const nothrow {
+  @nogc float[3][2] bounds(const Bounds[] world) const nothrow {
     Bounds b;
     foreach(i; first .. first + count) {
       if(i >= world.length) break;
-      b.update(world[i][0]); b.update(world[i][1]);
+      b.update(world[i]);
     }
     return [b.min, b.max];
   }
