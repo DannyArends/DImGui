7e2e6ebc71a0797c9380c0a3086453e1e955ceec
DannyArends <Danny.Arends@gmail.com>
2026-08-02 10:12:28 +0100

    Confusion sets in

--- src/math/frustum.d
+++ src/math/frustum.d
@@ -36,14 +36,7 @@ bool aabbInFrustum(const Plane[6] planes, const float[3] mn, const float[3] mx) 
 @nogc void cullFrustum(T)(ref T[] objects, const Plane[6] frustum) nothrow {
   for (size_t x = 0; x < objects.length; x++) {
     if(objects[x].box is null) continue;
-    bool any = false;
-    for (size_t i = 0; i < objects[x].box.instances.length; i++) {
-      auto b = (i < objects[x].box.world.length) ? objects[x].box.world[i] : objects[x].box.boundsWorld(i);
-      bool v = aabbInFrustum(frustum, b[0], b[1]);
-      if(i < objects[x].box.inFrustum.length) objects[x].box.inFrustum[i] = v;
-      any = any || v;
-    }
-    objects[x].box.visible = any;
+    objects[x].box.visible = aabbInFrustum(frustum, objects[x].box.wmin, objects[x].box.wmax);
   }
 }
 
